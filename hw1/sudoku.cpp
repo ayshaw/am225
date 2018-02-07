7f4ce2263de3b8ff5977cfe3f3fa2aaa3f50e825
Ada <ayshaw@g.harvard.edu>
2018-02-06 23:11:14 -0500

    your mom sucks dick adsf

--- hw1/sudoku.cpp
+++ hw1/sudoku.cpp
@@ -128,8 +128,8 @@ int main()
 					{0, 0, 0, 0, 0, 0, 0, 7, 4},
 					{0, 0, 5, 2, 0, 6, 3, 0, 0}};
 	if (SolveSudoku(grid) == true)
-		printGrid(grid);
-		printf('yourmom');
+		{printGrid(grid);
+		printf('yourmom');}
 	else
 		printf("No solution exists");
 
