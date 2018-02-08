c25928ba349a203af26bf5da3f573832ff7a3f0d
Ada <ayshaw@g.harvard.edu>
2018-02-08 09:39:38 -0500

    asdf

--- hw1/sudoku.cpp
+++ hw1/sudoku.cpp
@@ -71,7 +71,7 @@ int main()
 			return 0;
 	}	
 	time_req = clock() - time_req;
-	std::cout<< "it took "<< (float)time_req/CLOCKS_PER_SEC/n<< " seconds" << std::endl;	
+	std::cout<< "it took "<< (float)time_req/CLOCKS_PER_SEC/100<< " seconds" << std::endl;	
 }
 
 // fuck everything now
