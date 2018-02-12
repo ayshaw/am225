ef7ef3252a16279173003fc9b87cb49d99e33444
Ada <ayshaw@g.harvard.edu>
2018-02-12 11:56:17 -0500

    your mom

--- hw1/remainder_M.cpp
+++ hw1/remainder_M.cpp
@@ -39,7 +39,7 @@ double findingremainder(int k[n],int d[n], int B, int divisor,double &remainder)
 	// 	remainder=remainder-floor(remainder/divisor)*divisor;
 	// 	return remainder;
 	for (int i=0; i<n; i++){
-		remainder+=d[i]*B%divisor-d[i]*myPow(B%divisor,k[i])*divisor;
+		remainder+=d[i]*B%divisor-d[i]*(int)Pow((double)B%divisor,(double)k[i])*divisor;
 	}
 	remainder=remainder-floor(remainder/divisor)*divisor
 }
